7ba68dc034af1c0a145a7a7f2fd3bffb6a2fcb4f
FienonTV <71425419+FienonTV@users.noreply.github.com>
2021-03-10 14:02:15 +0100

    Bugfixing

--- web-ha-widanski-tinawi-loerken/buuk/models/models.js
+++ web-ha-widanski-tinawi-loerken/buuk/models/models.js
@@ -3,6 +3,7 @@ const autoren_model = require("../models/autoren.js");
 const buecher_model = require("../models/buecher.js");
 const reservierungen_model = require("../models/reservierungen.js");
 const standorte_model = require("../models/standorte.js");
+const reservierung_model = require("../models/reservierungen.js");
 
 function generate_values() {
     /* Autoren */
@@ -17,22 +18,47 @@ function generate_values() {
 
     /* Buecher */
 
-    var buch1 = new buecher_model.buch("MaFi 1", 484845741, "PI", 2005, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore explicabo minus ducimus blanditiis pariatur quasi autem quidem quae aperiam quam, assumenda quo exercitationem odio ullam. Aliquid laudantium corporis quae aut.", autor1, true);
-    var buch2 = new buecher_model.buch("Mafi 2", 484845741, "PI", 2005, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore explicabo minus ducimus blanditiis pariatur quasi autem quidem quae aperiam quam, assumenda quo exercitationem odio ullam. Aliquid laudantium corporis quae aut.", autor2, true);
-    var buch3 = new buecher_model.buch("MaFi 3", 484845741, "PI", 2005, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore explicabo minus ducimus blanditiis pariatur quasi autem quidem quae aperiam quam, assumenda quo exercitationem odio ullam. Aliquid laudantium corporis quae aut.", autor3, true);
+    var buch1 = new buecher_model.buch("Programmieren in Java 1", 484845741, "Programmierung", 2005, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore explicabo minus ducimus blanditiis pariatur quasi autem quidem quae aperiam quam, assumenda quo exercitationem odio ullam. Aliquid laudantium corporis quae aut.", autor1, true);
+    var buch2 = new buecher_model.buch("C, C++ und C#", 484845741, "Programmierung", 2005, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore explicabo minus ducimus blanditiis pariatur quasi autem quidem quae aperiam quam, assumenda quo exercitationem odio ullam. Aliquid laudantium corporis quae aut.", autor2, true);
+    var buch3 = new buecher_model.buch("Grundlagen der Theoretischen Informatik Teil 1", 484845741, "Theoretische Informatik", 2005, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore explicabo minus ducimus blanditiis pariatur quasi autem quidem quae aperiam quam, assumenda quo exercitationem odio ullam. Aliquid laudantium corporis quae aut.", autor3, true);
+    var buch4 = new buecher_model.buch("Mathematik für Informatiker 1 (Lineare Algebra)", 484845741, "Mathematik", 2009, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore explicabo minus ducimus blanditiis pariatur quasi autem quidem quae aperiam quam, assumenda quo exercitationem odio ullam. Aliquid laudantium corporis quae aut.", autor1, true);
+    var buch5 = new buecher_model.buch("Grundlagen der Theoretischen Informatik Teil 2", 484845741, "Theoretische Informatik", 2005, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore explicabo minus ducimus blanditiis pariatur quasi autem quidem quae aperiam quam, assumenda quo exercitationem odio ullam. Aliquid laudantium corporis quae aut.", autor3, true);
+    var buch6 = new buecher_model.buch("Programmieren in Java 2", 484845741, "Programmierung", 2005, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore explicabo minus ducimus blanditiis pariatur quasi autem quidem quae aperiam quam, assumenda quo exercitationem odio ullam. Aliquid laudantium corporis quae aut.", autor1, true);
+    var buch7 = new buecher_model.buch("Grundlegene Webtechnoligien", "48484547", "Webentwicklung", 2005, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore explicabo minus ducimus blanditiis pariatur quasi autem quidem quae aperiam quam, assumenda quo exercitationem odio ullam. Aliquid laudantium corporis quae aut.", autor2, true);
+    var buch8 = new buecher_model.buch("Datenbanken", 484845741, "Datenbanken", 2005, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore explicabo minus ducimus blanditiis pariatur quasi autem quidem quae aperiam quam, assumenda quo exercitationem odio ullam. Aliquid laudantium corporis quae aut.", autor3, true);
+    var buch9 = new buecher_model.buch("Grundlagen der Informationssicherheit", 484845741, "Cyber Security", 2009, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore explicabo minus ducimus blanditiis pariatur quasi autem quidem quae aperiam quam, assumenda quo exercitationem odio ullam. Aliquid laudantium corporis quae aut.", autor1, true);
+    var buch10 = new buecher_model.buch("Grundlagen der Theoretischen Informatik Teil 2", 484845741, "Theoretische Informatik", 2005, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore explicabo minus ducimus blanditiis pariatur quasi autem quidem quae aperiam quam, assumenda quo exercitationem odio ullam. Aliquid laudantium corporis quae aut.", autor3, true);
 
     buecher_model.buecher.push(buch1);
     buecher_model.buecher.push(buch2);
     buecher_model.buecher.push(buch3);
+    buecher_model.buecher.push(buch4);
+    buecher_model.buecher.push(buch5);
+    buecher_model.buecher.push(buch6);
+    buecher_model.buecher.push(buch7);
+    buecher_model.buecher.push(buch8);
+    buecher_model.buecher.push(buch9);
+    buecher_model.buecher.push(buch10);
+
 
     /* Ausleihen */
 
     var ausleihe1 = new ausleihen_model.ausleihe(buch1, new Date("2020-01-05T00:00:00"), new Date("2020-01-12T00:00:00"), false);
     var ausleihe2 = new ausleihen_model.ausleihe(buch2, new Date("2020-01-10T00:00:00"), new Date("2020-01-17T00:00:00"), true);
+    var ausleihe3 = new ausleihen_model.ausleihe(buch6, new Date("2020-01-10T00:00:00"), new Date("2020-01-17T00:00:00"), true);
+    var ausleihe4 = new ausleihen_model.ausleihe(buch4, new Date("2020-01-10T00:00:00"), new Date("2020-01-17T00:00:00"), false);
+    var ausleihe5 = new ausleihen_model.ausleihe(buch10, new Date("2020-01-10T00:00:00"), new Date("2020-01-17T00:00:00"), true);
 
     ausleihen_model.ausleihen.push(ausleihe1);
     ausleihen_model.ausleihen.push(ausleihe2);
+    ausleihen_model.ausleihen.push(ausleihe3);
+    ausleihen_model.ausleihen.push(ausleihe4);
+    ausleihen_model.ausleihen.push(ausleihe5);
 
+
+    /* Reservierungen */
+    var reservierung1 = new reservierung_model.reservierung(buch10, new Date("2020-01-10T00:00:00"), new Date("2020-01-17T00:00:00"));
+    reservierung_model.reservierungen.push(reservierung1);
     /* Standorte */
 
     var standort1 = new standorte_model.standort("Emil-Figge-Straße", "Emil-Figge-Str. 44", 44227, "Dortmund", "+49 (0231) 9112-4917", "+49 (0231) 9112-4922", [
@@ -60,7 +86,13 @@ function generate_values() {
     standort1.addBuch(buch1);
     standort2.addBuch(buch2);
     standort3.addBuch(buch3);
-
+    standort2.addBuch(buch4);
+    standort1.addBuch(buch5);
+    standort3.addBuch(buch6);
+    standort2.addBuch(buch7);
+    standort1.addBuch(buch8);
+    standort3.addBuch(buch9);
+    standort1.addBuch(buch10);
     standorte_model.standorte.push(standort1);
     standorte_model.standorte.push(standort2);
     standorte_model.standorte.push(standort3);
--- web-ha-widanski-tinawi-loerken/buuk/readme.md
+++ web-ha-widanski-tinawi-loerken/buuk/readme.md
@@ -4,115 +4,125 @@
 
 - ~~Dashboard~~
 - ~~Bücher~~
-    - ~~Buchdetails~~
-    - ~~Buch hinzufügen~~
+- ~~Buchdetails~~
+- ~~Buch hinzufügen~~
 - Autoren
-    - ~~Autordetails~~
-    - ~~Autor hinzufügen~~
+- ~~Autordetails~~
+- ~~Autor hinzufügen~~
 - ~~Standorte~~
-    - ~~Emil-Figge-Straße 44 (Detail)~~
-    - ~~Sonnenstraße (Detail)~~
-    - ~~Max-Ophüls-Platz (Detail)~~
-    - ~~Standort hinzufügen~~
+- ~~Emil-Figge-Straße 44 (Detail)~~
+- ~~Sonnenstraße (Detail)~~
+- ~~Max-Ophüls-Platz (Detail)~~
+- ~~Standort hinzufügen~~
 - ~~Persönliche~~
-    - ~~Mein Profil~~
-    - ~~Reservierungen~~
-        - ~~Reservierungsdetails~~
-        - ~~Reservierungen hinzufügen~~
-    - ~~Ausleihen~~
-        - ~~Ausleihendetails~~
-    - ~~Login~~
-    - ~~Logout~~
+- ~~Mein Profil~~
+- ~~Reservierungen~~
+- ~~Reservierungsdetails~~
+- ~~Reservierungen hinzufügen~~
+- ~~Ausleihen~~
+- ~~Ausleihendetails~~
+- ~~Login~~
+- ~~Logout~~
 
 ## Klassen
 
 - Buch
-    - name (string)
-    - isbn (int)
-    - fachrichtung (string)
-    - jahr (int)
-    - beschreibung (string)
-    - autor (array -> foreignkey)
-    - status (boolean)
+- name (string)
+- isbn (int)
+- fachrichtung (string)
+- jahr (int)
+- beschreibung (string)
+- autor (array -> foreignkey)
+- status (boolean)
 - Autor
-    - name (string)
-    - fachrichtung(String)
-    - Beschreibung(String)
+- name (string)
+- fachrichtung(String)
+- Beschreibung(String)
 - Kunde
-    - name (string)
-    - Straße (string)
-    - Stadt (string)
-    - Postleitzahl (int)
-    - typ (string)
-    - Fachrichtung(string)
+- name (string)
+- Straße (string)
+- Stadt (string)
+- Postleitzahl (int)
+- typ (string)
+- Fachrichtung(string)
 - Standort
-    - name (string)
-    - adresse (string)
-    - plz (int)
-    - ort (string)
-    - telefon (int)
-    - fax (int)
-    - öffnungszeiten (array -> string)
-    - ausstattung (array -> string)
-    - bücher (array -> foreignkey)
+- name (string)
+- adresse (string)
+- plz (int)
+- ort (string)
+- telefon (int)
+- fax (int)
+- öffnungszeiten (array -> string)
+- ausstattung (array -> string)
+- bücher (array -> foreignkey)
 - Reservierungen
-    - buch (foreignkey)
-    - datumVon (date)
-    - datumBis (date)
-    - token (string)
+- buch (foreignkey)
+- datumVon (date)
+- datumBis (date)
+- token (string)
 - Ausleihen
-    - buch (foreignkey)
-    - datumVon (date)
-    - datumBis (date)
-    - status (int)
+- buch (foreignkey)
+- datumVon (date)
+- datumBis (date)
+- status (int)
 
 ## Aufgaben
 
 - Allgemein
-    - ~~Menü aktiv schalten~~
+- ~~Menü aktiv schalten~~
 - Dashboard
-    - ~~Automatisierte Ausgabe~~
+- ~~Automatisierte Ausgabe~~
 - Standorte
-    - ~~Automatisierte Aufgabe der Übersicht~~
-    - ~~Automatisierte Ausgabe der Detailseite~~
-    - ~~Anlegen eines Standortes~~
-    - ~~Bücher auf der Detaisleite ausgeben~~
-    - ~~Ausgabe der Autoren in der Bücherübersicht~~
+- ~~Automatisierte Aufgabe der Übersicht~~
+- ~~Automatisierte Ausgabe der Detailseite~~
+- ~~Anlegen eines Standortes~~
+- ~~Bücher auf der Detaisleite ausgeben~~
+- ~~Ausgabe der Autoren in der Bücherübersicht~~
 - Reservierungen
-    - ~~Automatisierte Ausgabe der Übersicht~~
-    - ~~Automatisierte Ausgabe der Detailseite~~
-    - ~~Anlegen einer Reservierung~~
-    - ~~Löschen einer Reservierung~~
-    - ~~Genieriung des Tokens~~
+- ~~Automatisierte Ausgabe der Übersicht~~
+- ~~Automatisierte Ausgabe der Detailseite~~
+- ~~Anlegen einer Reservierung~~
+- ~~Löschen einer Reservierung~~
+- ~~Genieriung des Tokens~~
 - Ausleihen
-    - ~~Automatisierte Aufgabe der Übersicht~~
-    - ~~Automatisierte Ausgabe der Detailseite~~
+- ~~Automatisierte Aufgabe der Übersicht~~
+- ~~Automatisierte Ausgabe der Detailseite~~
 - Bücher
-    - ~~Automatisierte Aufgabe der Übersicht~~
-    - ~~Automatisierte Ausgabe der Detailseite~~
-    - ~~Anlegen eines Buches~~
-    - ~~Ausgabe des Autors in der Übersicht~~
-    - ~~Ausgabe des Standortes auf der Detailseite~~
+- ~~Automatisierte Aufgabe der Übersicht~~
+- ~~Automatisierte Ausgabe der Detailseite~~
+- ~~Anlegen eines Buches~~
+- ~~Ausgabe des Autors in der Übersicht~~
+- ~~Ausgabe des Standortes auf der Detailseite~~
 - Autoren
-    - ~~Automatisierte Aufgabe der Übersicht~~
-    - ~~Automatisierte Ausgabe der Detailseite~~
-    - ~~Anlegen eines Autors~~
+- ~~Automatisierte Aufgabe der Übersicht~~
+- ~~Automatisierte Ausgabe der Detailseite~~
+- ~~Anlegen eines Autors~~
 
 ## Wichtig: Ausstehende Aufgaben
 
-- Buch-Detail Seite: Section in Section ist nicht gut. Überschrift für jede Secttion hinzufügen!
+- ~~Buch-Detail Seite: Section in Section ist nicht gut. Überschrift für jede Secttion hinzufügen!~~
 - Alle Seiten W3C validieren (https://validator.w3.org/#validate_by_input)
 - CSS W3C validieren (https://jigsaw.w3.org/css-validator/#validate_by_input)
 - ~~Profil bearbeiten entfernen -> Input-Felder entfernen~~
 - ~~Ausleihen -> Jede Menge W3C-Fehler, u.a. doppelte IDs~~
-- Ausleihdetails -> Jede Menge W3C-Fehler, da jede Zeile eine Section ist. Unbedingt ändern!
+- ~~Ausleihdetails -> Jede Menge W3C-Fehler, da jede Zeile eine Section ist. Unbedingt ändern!~~
 - Alle Bilder lokal ablegen (in den meisten Fällen schon gemacht) -> Prüfen
 - ~~Impressum / Datenschutz muss ohne JavaScript laufen -> Eigene Seiten~~
+- Mehr Instanzen von jedem anlegen
+
+## Wichtig: BUGS
+
+- Mein Profil
+
+  - Bild wird kleiner aber nicht schrittweise sondern fließend
+
+- Reservierungen
+  - Hinzugefügte Daten im Quellcode und auf der Seite können nicht gleichzeitig angezeigt werden
 
 ## Zusatzaufwand:
 
 - Reservierungen
-    - Datum von kann später als Datum bis sein
-    - Wenn ein Buch für eine Zeit reserviert ist, kann es nicht nochmal in der gleichen Zeit ausgeliehen werden
-    - Wenn eine reservierung abgegeben wurde sollte man zu /user/reservierungen/detail weitergeleitet werden
-    - Daten der Reservierungen dürfen maximal 4 Wochen auseinander liegen (30 Tage)
+- Datum von kann später als Datum bis sein
+- Wenn ein Buch für eine Zeit reserviert ist, kann es nicht nochmal in der gleichen Zeit ausgeliehen werden
+- Wenn eine reservierung abgegeben wurde sollte man zu /user/reservierungen/detail weitergeleitet werden
+- Daten der Reservierungen dürfen maximal 4 Wochen auseinander liegen (30 Tage)
